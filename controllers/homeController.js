07e4b5c3ca43af35ad53bec910f16cde58fb6500
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-02-17 10:49:45 +0200

    Render Guest Home Page & User Home Page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,6 +3,7 @@ const homeController = require('express').Router();
 // TODO: replace with real controller 
 homeController.get('/', (req, res) => {
 	let view;
+	let courses = [];
 
 	if (req.user) {
 		view = 'user-home';
@@ -11,7 +12,8 @@ homeController.get('/', (req, res) => {
 	}
 
 	res.render(view, {
-		title: 'Home Page'
+		title: 'Home Page',
+		courses
 	});
 });
 

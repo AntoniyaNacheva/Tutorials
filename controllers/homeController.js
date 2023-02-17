8e4aa137309fd781e2968f5e69eaa1e50382a63f
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2023-02-17 12:36:27 +0200

    SORT Custom Model on Home Page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,4 +1,4 @@
-const { getAllByDate } = require('../services/courseService');
+const { getAllByDate, getRecent } = require('../services/courseService');
 
 const homeController = require('express').Router();
 
@@ -12,6 +12,7 @@ homeController.get('/', async (req, res) => {
 		courses = await getAllByDate();
 	} else {
 		view = 'guest-home';
+		courses = await getRecent();
 	}
 
 	res.render(view, {
--- services/courseService.js
+++ services/courseService.js
@@ -5,11 +5,16 @@ async function getAllByDate() {
 	return Course.find({}).sort({ createdAt: 1 }).lean();
 }
 
+async function getRecent() {
+	return Course.find({}).sort({userCount: -1}).limit().lean();
+}
+
 async function createCourse(course) {
-	return Course.create(course);
+	return Course.create(course);3
 }
 
 module.exports = {
 	getAllByDate,
+	getRecent,
 	createCourse
 };
